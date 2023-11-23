ac1aefd6e8b2edb0389951348b59fe7016363f24
Insaneoftelegram <104971406+Insaneoftelegram@users.noreply.github.com>
2023-11-23 14:07:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID","-1001768061227"))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Resso")
 
 OWNER_ID = list(map(int, getenv("OWNER_ID", "1356469075").split()))
